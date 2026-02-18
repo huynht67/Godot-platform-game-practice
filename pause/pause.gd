7db1b8ecf912d9b2d498653c0c11191656d3caac
Tuan Huynh <131468167+huynht67@users.noreply.github.com>
2026-02-17 23:40:47 -0600

    Added main menu and pause menu

--- pause/pause.gd
+++ pause/pause.gd
@@ -1,8 +1,17 @@
 extends Control
 
+@onready var resume_btn: Button = $PanelContainer/VBoxContainer/Resume
+
+@onready var restart_btn: Button = $PanelContainer/VBoxContainer/Restart
+
+@onready var quit_btn: Button = $PanelContainer/VBoxContainer/Quit
 func _ready():
 	#$AnimationPlayer.play("RESET")
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	hide()
+	resume_btn.pressed.connect(_on_resume_pressed)
+	restart_btn.pressed.connect(_on_restart_pressed)
+	quit_btn.pressed.connect(_on_quit_pressed)
 	
 func resume():
 	get_tree().paused = false
@@ -26,11 +35,12 @@ func _on_resume_pressed() -> void:
 
 
 func _on_restart_pressed() -> void:
-	resume()
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
 func _on_quit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().quit()
 
 func _process(delta):
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	print("Start pressed")
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game_new.tscn")
 
 
 func _on_options_pressed() -> void:
